1c6e5ceeb85d34b85750980957b34133b41ffbb8
jacob28 <jacob28@live.unc.edu>
2022-04-07 15:25:33 -0400

    asdhg;ldsaj

--- server.js
+++ server.js
@@ -3,21 +3,32 @@ const minimist = require("minimist")
 const args = minimist(process.argv.slice(2))
 
 args["port"]
-args["help"]
-args["h"]
 args["debug"]
 args["log"]
 
-const help = args.help;
-const h = args.h;
+// See what is stored in the object produced by minimist
+console.log(args)
+// Store help text 
+const help = (`
+server.js [options]
 
-if (help !== undefined) {
-    console.log(help);
-    process.exit(0);
-}
-if (h !== undefined) {
-    console.log(h);
-    process.exit(0);
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+// If --help or -h, echo help text to STDOUT and exit
+if (args.help || args.h) {
+    console.log(help)
+    process.exit(0)
 }
 
 const database = require('better-sqlite3');
