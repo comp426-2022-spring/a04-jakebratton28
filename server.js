aa6e471af251b1442a289c02a0d7e35a077e503f
jacob28 <jacob28@live.unc.edu>
2022-04-09 19:08:12 -0400

    Fixed old db error

--- server.js
+++ server.js
@@ -52,18 +52,18 @@ if (row === undefined) {
     console.log('Log database appears to be empty. Creating log database...')
 
     const sqlInit = `
-        CREATE TABLE accesslog (  
-            remoteaddr VARCHAR, 
-            remoteuser VARCHAR, 
-            time VARCHAR, 
-            method VARCHAR, 
-            url VARCHAR, 
-            protocol VARCHAR,
-            httpversion NUMERIC, 
-            status INTEGER, 
-            referer VARCHAR,
-            useragent VARCHAR
-        );
+    CREATE TABLE accesslog (  
+        remoteaddr VARCHAR, 
+        remoteuser VARCHAR, 
+        time VARCHAR, 
+        method VARCHAR, 
+        url VARCHAR, 
+        protocol VARCHAR,
+        httpversion NUMERIC, 
+        status INTEGER, 
+        referer VARCHAR,
+        useragent VARCHAR
+    );
     `
 
     logdb.exec(sqlInit)
